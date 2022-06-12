01224143cf30d7b4f2e35ae9dea161b587a0fa5b
SAI YUGANDHAR <ss19ahy@herts.ac.uk>
2022-06-12 02:01:33 +0100

    Initial commit

--- rdd/scholaship_recipient_join_filter.py
+++ rdd/scholaship_recipient_join_filter.py
@@ -48,4 +48,4 @@ if __name__ == '__main__':
 
     join_pair_rdd.foreach(print)
 
-# spark-submit --master yarn --packages "org.apache.hadoop:hadoop-aws:2.7.4" rdd/scholaship_recipient_join_filter.py
+# spark-submit -master yarn --packages "org.apache.hadoop:hadoop-aws:2.7.4" rdd/scholaship_recipient_join_filter.py
